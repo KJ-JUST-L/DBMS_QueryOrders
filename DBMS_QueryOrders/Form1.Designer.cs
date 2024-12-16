f5588afff5b64982518b8d85f855eaf134c93bcd
Just L <blacklistzaza@gmail.com>
2024-12-16 16:20:39 +0700

    Add project files.

---
+++ DBMS_QueryOrders/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace DBMS_QueryOrders
+{
+    partial class DBMS_QueryOrders
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvOrders = new DataGridView();
+            dgvDetails = new DataGridView();
+            label2 = new Label();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgvOrders).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvDetails).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvOrders
+            // 
+            dgvOrders.AllowUserToAddRows = false;
+            dgvOrders.AllowUserToDeleteRows = false;
+            dgvOrders.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvOrders.Location = new Point(37, 42);
+            dgvOrders.Name = "dgvOrders";
+            dgvOrders.ReadOnly = true;
+            dgvOrders.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvOrders.Size = new Size(728, 280);
+            dgvOrders.TabIndex = 0;
+            dgvOrders.CellMouseUp += dgvOrders_CellMouseUp;
+            // 
+            // dgvDetails
+            // 
+            dgvDetails.AllowUserToAddRows = false;
+            dgvDetails.AllowUserToDeleteRows = false;
+            dgvDetails.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDetails.Location = new Point(37, 351);
+            dgvDetails.Name = "dgvDetails";
+            dgvDetails.ReadOnly = true;
+            dgvDetails.Size = new Size(728, 234);
+            dgvDetails.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(37, 333);
+            label2.Name = "label2";
+            label2.Size = new Size(65, 15);
+            label2.TabIndex = 3;
+            label2.Text = "รายการสินค้า";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(37, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 15);
+            label1.TabIndex = 4;
+            label1.Text = "ใบสั่งซื้อ";
+            // 
+            // DBMS_QueryOrders
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(810, 618);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Controls.Add(dgvDetails);
+            Controls.Add(dgvOrders);
+            Name = "DBMS_QueryOrders";
+            Text = "QueryOrders";
+            Load += DBMS_QueryOrders_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvOrders).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvDetails).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dgvOrders;
+        private DataGridView dgvDetails;
+        private Label label2;
+        private Label label1;
+    }
+}
---
+++ DBMS_QueryOrders/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace DBMS_QueryOrders
+{
+    public partial class DBMS_QueryOrders : Form
+    {
+        public DBMS_QueryOrders()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+
+        public void connect()
+        {
+            string server = @".\SQLEXPRESS";
+            string db = "northwind";
+            string strConn = string.Format(@"Data Source={0};Initial Catalog={1};Integrated Security=True ;Encrypt=False", server, db);
+            conn = new SqlConnection(strConn);
+            conn.Open();
+        }
+        public void disconnect()
+        {
+            conn.Close();
+        }
+
+        private void showData(string sql, DataGridView dgv)
+        {
+            da = new SqlDataAdapter(sql, conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dgv.DataSource = ds.Tables[0];
+            dgvOrders.DataSource = ds.Tables[0];
+        }
+
+        private void DBMS_QueryOrders_Load(object sender, EventArgs e)
+        {
+            connect();
+            string sqlQuery = "SELECT o.OrderID, o.OrderDate, FORMAT(o.RequiredDate, 'dd-MM-yyyy'), s.CompanyName AS ShipperCompany,  CONCAT(e.TitleOfCourtesy, e.FirstName, ' ', e.LastName) as employeeName, c.CompanyName AS CustomerCompany,  c.Phone,  SUM(od.Quantity * od.UnitPrice * (1 - od.Discount)) AS totalCash FROM [Order Details] od JOIN Orders o ON o.OrderID = od.OrderID  JOIN Shippers s ON s.ShipperID = o.ShipVia  JOIN Employees e ON e.EmployeeID = o.EmployeeID  JOIN Customers c ON c.CustomerID = o.CustomerID GROUP BY  o.OrderID,  o.OrderDate, o.RequiredDate,  s.CompanyName, CONCAT(e.TitleOfCourtesy, e.FirstName, ' ', e.LastName), c.CompanyName, c.Phone";
+            showData(sqlQuery, dgvOrders);
+        }
+
+        private void dgvOrders_CellMouseUp(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.ColumnIndex != 0)
+            {
+                int id = Convert.ToInt32(dgvOrders.CurrentRow.Cells[0].Value);
+                //MessageBox.Show(id.ToString(), "รหัสที่เลือก");
+                string sqlQuery = "SELECT p.ProductID, p.ProductName, od.Quantity, od.UnitPrice, (od.Discount * 100) AS Discount, CONVERT(decimal(10, 2), od.Quantity * od.UnitPrice * (1 - od.Discount)) AS totalSellPrice, od.Quantity * od.UnitPrice * od.Discount AS totalDiscount, CONVERT(decimal(10, 2), (od.Quantity * od.UnitPrice * (1 - od.Discount)) - od.Quantity * od.UnitPrice * od.Discount) AS totalExtraCharge FROM [Order Details] od JOIN Orders o ON o.OrderID = od.OrderID JOIN Products p ON p.ProductID = od.ProductID WHERE o.OrderID = @id";
+                cmd = new SqlCommand(sqlQuery, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                dgvDetails.DataSource = ds.Tables[0];
+            }
+        }
+    }
+}
